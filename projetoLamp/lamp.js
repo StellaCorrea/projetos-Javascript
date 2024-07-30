f51f4a82ef63c5529ea031608d2ee4e595127545
StellaCorrea <ster_muniz@hotmail.com>
2024-07-30 20:16:20 -0300

    ajustes

---
+++ projetoLamp/lamp.js
@@ -0,0 +1,16 @@
+    const turnOn = document.getElementById( 'turnOn');
+    const turnOff = document.getElementById('turnOff');
+    const lamp = document.getElementById('lamp');
+
+
+    function lampOn () {
+        lamp.src = './img/ligada.jpg';
+
+
+    }
+
+    function lampOff () {
+        lamp.src = './img/desligada.jpg';
+    }
+    turnOn.addEventListener('click', lampOn);
+    turnOn.addEventListener('click', lampOff);
